a8d0493f29285694f2653f3041dcc431b22de063
Keanu Rocha Anse <keanu.anse@Hotmail.com>
2025-12-10 21:40:40 -0300

    Add files via upload

---
+++ 1115.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int x, y;
+
+    while (scanf("%d %d", &x, &y) && x != 0 && y != 0) {
+        if (x > 0 && y > 0) {
+            printf("primeiro\n");
+        } else if (x < 0 && y > 0) {
+            printf("segundo\n");
+        } else if (x < 0 && y < 0) {
+            printf("terceiro\n");
+        } else {
+            printf("quarto\n");
+        }
+    }
+
+    return 0;
+}
