517bd68775a85b4052cb0d086412e09cff82d75b
Keanu Rocha Anse <keanu.anse@Hotmail.com>
2026-01-08 16:46:13 -0300

    Update 1566.heapsort.c

--- 1566.heapsort.c
+++ 1566.heapsort.c
@@ -1,63 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void swap(int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
+void heapify(int v[], int n, int i) {
+    int maior, esq, dir, tmp;
+    
+    while (1) {
+        maior = i;
+        esq = 2 * i + 1;
+        dir = 2 * i + 2;
 
-void heapify(int arr[], int n, int i) {
-    int largest = i;
-    int left = 2 * i + 1;
-    int right = 2 * i + 2;
+        if (esq < n && v[esq] > v[maior])
+            maior = esq;
+        if (dir < n && v[dir] > v[maior])
+            maior = dir;
 
-    if (left < n && arr[left] > arr[largest])
-        largest = left;
-
-    if (right < n && arr[right] > arr[largest])
-        largest = right;
-
-    if (largest != i) {
-        swap(&arr[i], &arr[largest]);
-        heapify(arr, n, largest);
+        if (maior != i) {
+            tmp = v[i];
+            v[i] = v[maior];
+            v[maior] = tmp;
+            i = maior;
+        } else {
+            break;
+        }
     }
 }
 
-void heapSort(int arr[], int n) {
-    for (int i = n / 2 - 1; i >= 0; i--)
-        heapify(arr, n, i);
+void heapSort(int v[], int n) {
+    int i, tmp;
+    for (i = n / 2 - 1; i >= 0; i--)
+        heapify(v, n, i);
 
-    for (int i = n - 1; i > 0; i--) {
-        swap(&arr[0], &arr[i]);
-        heapify(arr, i, 0);
+    for (i = n - 1; i > 0; i--) {
+        tmp = v[0];
+        v[0] = v[i];
+        v[i] = tmp;
+        heapify(v, i, 0);
     }
 }
 
-int main() {
-    int NC;
-    scanf("%d", &NC);
+int main(void) {
+    int t, n, i;
+    if (scanf("%d", &t) != 1) return 0;
 
-    while (NC--) {
-        int N;
-        scanf("%d", &N);
+    while (t--) {
+        if (scanf("%d", &n) != 1) break;
 
-        int *alturas = (int *)malloc(N * sizeof(int));
-        if (alturas == NULL) {
-            return 1;
+        int *v = (int *)malloc(n * sizeof(int));
+        
+        for (i = 0; i < n; i++)
+            scanf("%d", &v[i]);
+
+        heapSort(v, n);
+
+        for (i = 0; i < n; i++) {
+            printf("%d%s", v[i], (i == n - 1) ? "" : " ");
         }
-
-        for (int i = 0; i < N; i++) {
-            scanf("%d", &alturas[i]);
-        }
-
-        heapSort(alturas, N);
-
-        for (int i = 0; i < N; i++) {
-            printf("%d%c", alturas[i], (i == N - 1) ? '\n' : ' ');
-        }
-
-        free(alturas);
+        printf("\n");
+        
+        free(v);
     }
 
     return 0;
