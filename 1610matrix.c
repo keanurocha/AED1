cf1c997cd4e3424bc6451aa17e9f45e6a308b3b0
Keanu Rocha Anse <keanu.anse@Hotmail.com>
2026-01-08 17:03:47 -0300

    Refactor cycle detection in directed graph

--- 1610matrix.c
+++ 1610matrix.c
@@ -1,67 +1,59 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
-#define MAX_N 10005
+#define MAX_SIZE 10005
 
-// Matriz de adjacência (usando char para economizar memória)
-// adj[i][j] = 1 se 'i' depende de 'j'
-char adj[MAX_N][MAX_N];
-int vis[MAX_N]; // 0: Branco, 1: Cinza, 2: Preto
-int N, M;
+char grade[MAX_SIZE][MAX_SIZE];
+int status[MAX_SIZE];
+int v_num, a_num;
 
-// Retorna 1 se encontrou ciclo, 0 caso contrário
-int dfs(int u) {
-    vis[u] = 1; // Marca como "Visitando" (Cinza)
+int verificar_circuito(int ponto) {
+    status[ponto] = 1;
 
-    for (int v = 1; v <= N; v++) {
-        if (adj[u][v]) {
-            if (vis[v] == 1) {
-                return 1; // Ciclo detectado (encontrou nó cinza)
+    for (int vizinho = 1; vizinho <= v_num; vizinho++) {
+        if (grade[ponto][vizinho]) {
+            if (status[vizinho] == 1) {
+                return 1;
             }
-            if (vis[v] == 0) {
-                if (dfs(v)) return 1;
+            if (status[vizinho] == 0) {
+                if (verificar_circuito(vizinho)) return 1;
             }
         }
     }
 
-    vis[u] = 2; // Marca como "Finalizado" (Preto)
+    status[ponto] = 2;
     return 0;
 }
 
 int main() {
-    int T;
-    if (scanf("%d", &T) != 1) return 0;
+    int testes;
+    if (scanf("%d", &testes) != 1) return 0;
 
-    while (T--) {
-        scanf("%d %d", &N, &M);
+    for (int t = 0; t < testes; t++) {
+        if (scanf("%d %d", &v_num, &a_num) != 2) break;
 
-        // Limpeza das estruturas
-        // Nota: Limpar uma matriz 10000x10000 é lento. 
-        // Em um cenário real de competição, evite matriz para N grande.
-        for (int i = 0; i <= N; i++) {
-            vis[i] = 0;
-            // Otimização: limpar apenas as linhas usadas ou usar memset se N for pequeno
-            memset(adj[i], 0, (N + 1) * sizeof(char)); 
+        for (int i = 0; i <= v_num; i++) {
+            status[i] = 0;
+            memset(grade[i], 0, (v_num + 1) * sizeof(char));
         }
 
-        for (int i = 0; i < M; i++) {
-            int u, v;
-            scanf("%d %d", &u, &v);
-            adj[u][v] = 1;
+        for (int i = 0; i < a_num; i++) {
+            int de, para;
+            scanf("%d %d", &de, &para);
+            grade[de][para] = 1;
         }
 
-        int ciclo = 0;
-        for (int i = 1; i <= N; i++) {
-            if (vis[i] == 0) {
-                if (dfs(i)) {
-                    ciclo = 1;
+        int achou_loop = 0;
+        for (int i = 1; i <= v_num; i++) {
+            if (status[i] == 0) {
+                if (verificar_circuito(i)) {
+                    achou_loop = 1;
                     break;
                 }
             }
         }
 
-        if (ciclo) printf("SIM\n");
+        if (achou_loop) printf("SIM\n");
         else printf("NAO\n");
     }
 
