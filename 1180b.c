66bf469f2cc6b4fab52af1e4fb01b78b6a21e3b1
Keanu Rocha Anse <keanu.anse@Hotmail.com>
2026-01-08 16:31:22 -0300

    Refactor code to find minimum value in an array

--- 1180b.c
+++ 1180b.c
@@ -1,57 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct No {
-    int valor;
-    int indiceOriginal;
-    struct No *proximo;
-} No;
-
-void push(No **topo, int val, int idx) {
-    No *novo = (No*)malloc(sizeof(No));
-    novo->valor = val;
-    novo->indiceOriginal = idx;
-    novo->proximo = *topo;
-    *topo = novo;
-}
-
-No* pop(No **topo) {
-    if (*topo == NULL) return NULL;
-    No *aux = *topo;
-    *topo = (*topo)->proximo;
-    return aux;
-}
-
 int main() {
-    int N, val, i;
-    No *pilha = NULL;
-    
-    scanf("%d", &N);
-    
-    // Empilha todos os elementos
-    for (i = 0; i < N; i++) {
-        scanf("%d", &val);
-        push(&pilha, val, i);
+    int n, i, pos;
+    int *v;
+
+    if (scanf("%d", &n) != 1) return 0;
+
+    v = (int*) malloc(n * sizeof(int));
+
+    for (i = 0; i < n; i++) {
+        scanf("%d", (v + i));
     }
-    
-    // Processa o primeiro elemento desempilhado
-    No *temp = pop(&pilha);
-    int menor = temp->valor;
-    int posicao = temp->indiceOriginal;
-    free(temp);
-    
-    // Desempilha o restante comparando
-    while (pilha != NULL) {
-        temp = pop(&pilha);
-        if (temp->valor < menor) {
-            menor = temp->valor;
-            posicao = temp->indiceOriginal;
+
+    int m = *v;
+    pos = 0;
+
+    for (i = 1; i < n; i++) {
+        if (*(v + i) < m) {
+            m = *(v + i);
+            pos = i;
         }
-        free(temp);
     }
-    
-    printf("Menor valor: %d\n", menor);
-    printf("Posicao: %d\n", posicao);
-    
+
+    printf("Menor valor: %d\n", m);
+    printf("Posicao: %d\n", pos);
+
+    free(v);
+
     return 0;
 }
